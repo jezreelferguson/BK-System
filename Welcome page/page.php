e3cabca551972881c5dbeab498d7011df5ad991b
Ferguson Jezreel <anojafa24@gmail.com>
2025-02-21 13:19:19 +0000

    page.php edit

--- Welcome page/page.php
+++ Welcome page/page.php
@@ -3,13 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="page.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Login Popup</title>
-    <style>
-       
-    </style>
 </head>
 <body>
     <h1>Welcome to BK System</h1>
@@ -17,7 +15,7 @@
     
     <div class="popup" id="loginPopup">
         <button onclick="alert('Client Login')">Login as Client</button>
-        <button onclick="alert('Admin Login')">Login as Admin</button>
+        <a href="BankSystem/index.php"><button onclick="alert('Admin Login')">Login as Admin</a></button>
         <button onclick="closePopup()">x</button>
     </div>
     
